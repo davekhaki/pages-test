8ab711a37a181e9056dc2881652ffbf0ded8eb64
Dave <david.la.gordt.dillie@gmail.com>
2021-07-07 13:52:44 +0200

    edit app.js test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>facts</p>
         <a
           className="App-link"
           href="https://reactjs.org"
